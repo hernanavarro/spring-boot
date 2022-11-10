5960f84772a31721631b79ea7ec3e8b6d3f808b8
DESKTOP-FLF0E7C\herna <navarro.hernan97@gmail.com>
2022-11-10 02:12:15 -0300

    Add more test

--- src/test/java/sample/actuator/HelloWorldServiceTest.java
+++ src/test/java/sample/actuator/HelloWorldServiceTest.java
@@ -1,6 +1,8 @@
 package sample.actuator;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import org.junit.Test;
 
@@ -11,5 +13,15 @@ public class HelloWorldServiceTest {
 		HelloWorldService helloWorldService = new HelloWorldService();
 		assertEquals("Expected correct message","Spring boot says hello from a Docker container",helloWorldService.getHelloMessage());
 	}
-	
+
+	@Test
+	public void secondTest() {
+		HelloWorldService serviceMock = mock(HelloWorldService.class);
+
+		when(serviceMock.getHelloMessage()).thenReturn("Hola Hola").thenReturn("Hello Hello");
+
+		assertEquals("Expected correct message", "Hola Hola", serviceMock.getHelloMessage());
+		assertEquals("Expected correct message", "Hello Hello", serviceMock.getHelloMessage());
+	}
+
 }
